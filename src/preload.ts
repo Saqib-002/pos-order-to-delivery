d672e3f1e882c6a0347ca5d1f0a76ab0d8636de0
Saqib-002 <saqibshah2882@gmail.com>
2025-08-20 06:26:38 +0500

    updated .gitignore

--- src/preload.ts
+++ src/preload.ts
@@ -1,9 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 
-// Note: We can't directly import db here as preload runs in a different context
-// Instead, we need to communicate through IPC
-
 contextBridge.exposeInMainWorld('electronAPI', {
   saveOrder: (order: any) => ipcRenderer.invoke('save-order', order),
   getOrders: () => ipcRenderer.invoke('get-orders'),
