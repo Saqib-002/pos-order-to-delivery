7a9140c15f05b76d416cd7005406fb4cf3a34693
Saqib-002 <saqibshah2882@gmail.com>
2025-08-20 07:14:12 +0500

    fix the building os not given error

--- forge.config.ts
+++ forge.config.ts
@@ -13,7 +13,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      config: {},
     },
     {
       name: '@electron-forge/maker-deb',
