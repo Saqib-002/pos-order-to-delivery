d00de4f2d686aa2de947bd0bc85d95758bae80e2
Saqib-002 <saqibshah2882@gmail.com>
2025-08-26 01:57:34 +0500

    changed the listener logic

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -6,6 +6,6 @@ import {ToastContainer} from "react-toastify";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ToastContainer position='bottom-right'/>
+    <ToastContainer position='bottom-right' autoClose={10000}/>
   </React.StrictMode>
 );
