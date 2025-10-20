6652ff9e6590155426cb1e7bb4a26acd0d43cb3b
Saqib-002 <saqibshah2882@gmail.com>
2025-10-20 14:48:20 +0500

    changed the builder

--- client/forge.config.ts
+++ client/forge.config.ts
@@ -1,49 +1,58 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+// client/forge.config.ts
+
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
-  packagerConfig: {
-    asar: true,
-    extraResource: [
-      '.env',
-      'migrations'
-    ]
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+    packagerConfig: {
+        asar: true,
+        extraResource: [".env", "migrations"],
     },
-    {
-      name: '@electron-forge/maker-zip',
-      config: {},
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
-  ],
+    rebuildConfig: {},
+    makers: [
+        {
+            // Windows Installer (NSIS) - *No* external toolset required
+            name: "@felixrieseberg/electron-forge-maker-nsis",
+            config: {
+                nsis:{
+                    oneClick: false,
+                    allowToChangeInstallationDirectory: true,
+                }
+            },
+            platforms: ["win32"],
+        },
+        {
+            // macOS Installer (DMG) - Standard Mac installer format
+            name: "@electron-forge/maker-dmg",
+            config: {
+                // DMG installers allow drag-and-drop to any directory
+            },
+            platforms: ["darwin"],
+        },
+        {
+            name: "@electron-forge/maker-deb",
+            config: {},
+        },
+        {
+            name: "@electron-forge/maker-rpm",
+            config: {},
+        },
+    ],
+    plugins: [
+        {
+            name: "@electron-forge/plugin-auto-unpack-natives",
+            config: {},
+        },
+        // Fuses are used to enable/disable various Electron functionality
+        // at package time, before code signing the application
+        new FusesPlugin({
+            version: FuseVersion.V1,
+            [FuseV1Options.RunAsNode]: false,
+            [FuseV1Options.EnableCookieEncryption]: true,
+            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+            [FuseV1Options.EnableNodeCliInspectArguments]: false,
+            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+            [FuseV1Options.OnlyLoadAppFromAsar]: true,
+        }),
+    ],
 };
